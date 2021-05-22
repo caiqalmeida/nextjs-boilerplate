56d6a2c286ab79854cd8923dc4cbb19900b7f09b
Caique Almeida <caaguima@gmail.com>
2021-05-22 19:45:58 -0300

    Bootstraped and installed typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = "Next-js Boilerplate" }: Props) {
+  return (
+    <div className="">
+      <h1>{title}</h1>
+    </div>
+  );
+}
